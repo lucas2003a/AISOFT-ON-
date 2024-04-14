0dbe05f074fbb7a832fbc612a8820d2c35ef7b4f
lucas2003a <lucasatuncar1@gmail.com>
2024-04-14 18:15:56 -0500

    reporte PDF

--- reports/reportLots_pdf.php
+++ reports/reportLots_pdf.php
@@ -10,6 +10,7 @@
  * @copyright 2023 Laurent MINGUET
  */
 require_once "../vendor/autoload.php";
+require_once "../Models/Asset.php";
 
 use Spipu\Html2Pdf\Html2Pdf;
 use Spipu\Html2Pdf\Exception\Html2PdfException;
@@ -19,10 +20,16 @@ try {
     // get the HTML
     ob_start();
     include "./reports_styles.html";
+    $asset = new Asset();
+
+    $idproyecto = 1;
+    $data = $asset->getLotsReports($idproyecto);
+
     include "./reportLots_pdf_content.php";
+
     $content = ob_get_clean();
 
-    $html2pdf = new Html2Pdf('P', 'A4', 'fr');
+    $html2pdf = new Html2Pdf('P', 'A4', 'es', true, 'UTF-8', array(15, 5, 15, 5));
     $html2pdf->pdf->SetDisplayMode('fullpage');
     $html2pdf->writeHTML($content);
     $html2pdf->output('example07.pdf');
--- reports/reportLots_pdf_content.php
+++ reports/reportLots_pdf_content.php
@@ -1,35 +1,95 @@
+
 <!DOCTYPE html>
-<html lang="es">
-<head>
+<html lang="en">
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reporte de lotes</title>
+  <title>Document</title>
 </head>
 <body>
-    <div class="d-flex tc content-center">
-        <h1><strong>A.I.F Contratistas generales S.A.C</strong></h1>
-        <h4 class="mt-1">Reporte de estados de lotes - Proyecto -codigo</h4>
-    </div>
+  
+<div class="container">
+  
+<?php
+  $denominacion = "";
+  $lotesVend = 0;
+  $lotesNoVend;
+  $lotesSep = 0;
+  $total = 0;
 
-    <table class="table mt-3">
-        <thead>
-            <tr>
-                <th>1</th>
-                <th>1</th>
-                <th>1</th>
-                <th>1</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>2222</td>
-                <td>2222</td>
-                <td>2222</td>
-                <td>2222</td>
-            </tr>
-        </tbody>
+  foreach($data as $register){
+    
+    $denominacion = $register["denominacion"];
+    $lotesVend = $register["l_vendidos"];
+    $lotesNoVend = $register["l_noVendidos"];
+    $lotesSep = $register["l_separados"];
+    $total = $register["l_total"];
+  }
+?>
+  <div class="d-flex tc content-center">
+    <h1><strong>A.I.F Contratistas generales S.A.C</strong></h1>
+    <hr>
+    <h4 class="mt-1">Reporte de estados de lotes - <?=$denominacion?></h4>
+  </div>
+  
+  <div></div>
+  <div>
+  </div>
+
+  <table class="table border-collapse tc">
+      <tbody>
+        <tr>
+          <td><strong>Lotes vendidos</strong></td>
+          <td><?=$lotesVend?></td>
+        </tr>
+        <tr>
+          <td><strong>Lotes no vendidos</strong></td>
+          <td><?=$lotesNoVend?></td>
+        </tr>
+        <tr>
+          <td><strong>Lotes separados</strong></td>
+          <td><?=$lotesSep?></td>
+        </tr>
+        <tr>
+          <td><strong>Total</strong></td>
+          <td><?=$total?></td>
+        </tr>
+      </tbody>
+  </table>
+
+  <div>
+  <div>
+  </div>
+  <div>
+
+    <table class="table border-collapse tc">
+      <thead>
+        <tr>
+          <th style="width:20%;">Sublote</th>
+          <th style="width:20%;">Estado</th>
+          <th style="width:20%;">moneda_venta</th>
+          <th style="width:20%;">precio_venta</th>
+          <th style="width:20%;">area_terreno</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Emil</td>
+          <td>Tobias</td>
+          <td>Linus</td>
+        </tr>
+        <tr>
+          <td>16</td>
+          <td>14</td>
+          <td>10</td>
+        </tr>
+      </tbody>
     </table>
-    <div>
-    </div>
+
+  </div>
+  </div>
+</div>
+  
 </body>
 </html>
+
