a6055e5d808371fc1fcc345dfe75716716899909
lucas2003a <lucasatuncar1@gmail.com>
2024-05-26 18:21:37 -0500

    backup

--- assets/database/aisoft(BACKUP)-8.sql
+++ assets/database/aisoft(BACKUP)-8.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-05-2024 a las 01:17:20
+-- Tiempo de generación: 27-05-2024 a las 01:20:37
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
