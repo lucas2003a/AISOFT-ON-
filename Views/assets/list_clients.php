8759a913a1fd58c8f36c1ef208cb562ff846f985
lucas2003a <lucasatuncar1@gmail.com>
2024-07-18 15:00:09 -0500

    restore

--- Views/assets/list_clients.php
+++ Views/assets/list_clients.php
@@ -144,10 +144,10 @@
                   </div>
                 </div>
 
-                <div class="col-md-6">
-                  <div class="row">
-                    <div class="col-md-6">
-
+                <div class="col-md-6">    
+                  <div class="row"> 
+                    <div class="col- md-6">
+ 
                     </div>
                     <div class="col-md-6">
 
